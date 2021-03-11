d927b636794485add6afd6c4bc5601c16bd1769e
Mauricio Gouveia <mauricioargj@gmail.com>
2021-03-11 01:53:02 -0300

    quarto commit

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -7,4 +7,23 @@ from visitantes.models import Visitante
 class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = [
+            "nome_completo", "cpf", "data_nascimento", "numero_casa",
+            "placa_veiculo"
+        ]
+
+        error_messages = {
+            "nome_completo": {
+                "required": "O Nome Completo do Visitante é Obrigatório para o Registro."
+            },
+            "cpf": {
+                "required": "O CPF do Visitante é Obrigatório para o Registro."
+            },
+            "data_nascimento": {
+                "required": "A Data de Nascimento do Visitante é Obrigatória para o Registro.",
+                "invalid": "Por Favor, Informe um Formato Válido para a Data de Nascimento. (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por Favor, Informe o Número da Casa a ser Visitada."
+            }
+        }
--- visitantes/views.py
+++ visitantes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from visitantes.forms import VisitanteForm
 
 
@@ -6,9 +7,34 @@ def registrar_visitante(request):
 
     form = VisitanteForm()
 
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            messages.success(
+                request,
+                "Visitante Registrado com Sucesso!"
+            )
+
+            return redirect("index")
+
     context = {
         "nome_pagina": "Registrar Visitante",
         "form": form
     }
 
     return render(request, "registrar_visitante.html", context)
+
+
+def informacoes_visitantes(request, id):
+
+    context = {
+        "nome_pagina": "Informações de Visitante",
+
+    }
+
+    return render(request, "informacoes_visitante.html", context)
